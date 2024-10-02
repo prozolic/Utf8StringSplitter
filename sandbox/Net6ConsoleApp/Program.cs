bb3290204f74b5010ce6f66a7737b81c99b5fc0c
prozolic <42107886+prozolic@users.noreply.github.com>
2024-10-02 23:45:12 +0900

    update

--- sandbox/Net6ConsoleApp/Program.cs
+++ sandbox/Net6ConsoleApp/Program.cs
@@ -1,22 +1,81 @@
-﻿using System.Text;
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
 using Utf8StringSplitter;
 
-Console.WriteLine("Hello, World!");
 
-foreach(var s in Utf8Splitter.Split("1,2,3,4,5"u8, ","u8))
+foreach (var s in Utf8Splitter.Split("     ,2, 3,4,5"u8, ","u8, Utf8StringSplitOptions.None))
 {
-    Console.WriteLine(Encoding.UTF8.GetString(s));
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
 }
-Console.WriteLine("--------------------");
+
+
+foreach (var s in Utf8Splitter.Split("   , 2  , [3] , 4, 5 6 "u8, ","u8, Utf8StringSplitOptions.TrimEntries))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
+
+foreach (var s in Utf8Splitter.Split(",,    ,4,   5, 6 7 ,,   "u8, ","u8, Utf8StringSplitOptions.RemoveEmptyEntries))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
+foreach (var s in Utf8Splitter.Split("  ,2 ,  ,, 5  6 "u8, ","u8, Utf8StringSplitOptions.TrimEntries | Utf8StringSplitOptions.RemoveEmptyEntries))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
 foreach (var s in Utf8Splitter.SplitAny("1;2-3,4-5"u8, ",-;"u8))
 {
-    Console.WriteLine(Encoding.UTF8.GetString(s));
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
 }
+
+foreach (var s in Utf8Splitter.SplitAny("1; 2- 3 ,4- 5,,,6,"u8, ",-;"u8, Utf8StringSplitOptions.TrimEntries | Utf8StringSplitOptions.RemoveEmptyEntries))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
+foreach (var s in Utf8Splitter.SplitAny("あいうえうお"u8, "う"u8))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
+foreach (var s in Utf8Splitter.SplitAny("あいうえうお"u8, "う"u8, delimiterOptions:Utf8StringDelimiterOptions.SingleByte))
+{
+    Console.WriteLine($"[{Encoding.UTF8.GetString(s)}]");
+}
+
+foreach (var s in "".Split(""))
+{
+    Console.WriteLine($"[{s}]");
+}
+
+foreach (var s in "   , 2  , [3] , 4,5  ".Split(",", StringSplitOptions.TrimEntries))
+{
+    Console.WriteLine($"[{s}]");
+}
+
+
+foreach (var s in ",,    ,4,   5, 6 ,,  , , ,  , , , , , , , , , , , ,,  , ".Split(",", StringSplitOptions.RemoveEmptyEntries))
+{
+    Console.WriteLine($"[{s}]");
+}
+
+
+foreach (var s in "  ,2 ,  ,,5   ".Split(",", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+{
+    Console.WriteLine($"[{s}]");
+}
+
+
 Console.WriteLine("--------------------");
 
-ReadOnlySpan<char> source = "apple,banana;cherry".AsSpan();
-Span<Range> destination = stackalloc Range[3];
-ReadOnlySpan<char> separators = ",;".AsSpan();
+ReadOnlySpan<char> source = "あいうえお".AsSpan();
+Span<Range> destination = stackalloc Range[100];
+ReadOnlySpan<char> separators = "う".AsSpan();
 
 int count = MemoryExtensions.SplitAny(source, destination, separators, StringSplitOptions.None);
 
